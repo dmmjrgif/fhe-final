cfdda22458f1e08ae158c80f48493101cee6d9d3
dmmjrgif <onlydipta@gmail.com>
2026-02-18 17:07:03 +0530

    Simplify Dockerfile structure

--- complete_fhe_package/setup.py
+++ complete_fhe_package/setup.py
@@ -4,7 +4,7 @@ Setup script for Custom FHE Library
 
 from setuptools import setup, find_packages
 
-with open("complete_fhe_package/README.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
